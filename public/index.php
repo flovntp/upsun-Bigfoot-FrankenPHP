560104fb949c0e74e6349510073930292e9d6a4e
Florent Huck <flovntp@gmail.com>
2024-03-07 09:14:35 +0100

    setting root for frankenPHP

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-//use App\Kernel;
-//
-//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-//
-//return function (array $context) {
-//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-//};
-echo phpinfo();
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
